8f3b003ed005f54cb4117cf231ed51e764fa7c3a
Eric <eytam619@gmail.com>
2023-03-13 21:34:12 -0400

    feat: implement skeleton files

---
+++ server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+const cors = require("cors");
+
+const port = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`)
+});

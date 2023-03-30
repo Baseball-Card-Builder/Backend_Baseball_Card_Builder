c0c9a0df2aef7a904068d224685ef12965182b62
Eric <eytam619@gmail.com>
2023-03-29 20:55:55 -0400

    update: removed username from users table

---
+++ db/data/migrations/20230330005057_delete_username_from_users_table.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    knex.schema.table('users', function(table) {
+        table.dropColumn('username');
+      });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable("users");
+};

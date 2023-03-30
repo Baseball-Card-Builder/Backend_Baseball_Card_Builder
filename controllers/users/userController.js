19761ba28d2bf219ecd2ec7711f085e9aaa53521
Eric <eytam619@gmail.com>
2023-03-29 22:16:51 -0400

    update: signup in controller

--- controllers/users/userController.js
+++ controllers/users/userController.js
@@ -6,43 +6,29 @@ const saltRounds = 10;
 /* respond to a post request to API_URL/user/signup */
 const signup = async (req, res) => {
   try {
-    const { username, password, email } = req.body;
+    const { password, email } = req.body;
 
-    if (!(email && password && username)) {
+    if (!(email && password)) {
       res.status(400).send("All input is required");
-    }
-
-    const existingUser = await User.findOne({ email });
-
-    if (existingUser) {
-        return res.status(409).send("User already exists. Please log in.")
     }
 
     const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-    const userArray = await knex("users")
-      .returning(["id", "username", "email"])
-      .insert({
-        email,
-        password: hashedPassword,
-        username,
-      });
+    const userArray = await knex("users").returning("*").insert({
+      email: email.toLowerCase(),
+      password: hashedPassword,
+    });
 
     const token = auth.createToken(userArray[0].id);
+    userArray.token = token;
 
-    const loginObject = {
-      token,
-      username: userArray[0].username,
-      email: userArray[0].email,
-    };
-
-    return res.status(201).send(json(loginObject));
+    return res.status(201).send(json(userArray));
   } catch (error) {
     return error;
   }
 };
 /* */
-const login = async(req, res) => {
+const login = async (req, res) => {
   try {
     const { email, password } = req.body;
 
@@ -51,27 +37,29 @@ const login = async(req, res) => {
     }
 
     const userArray = await knex
-      .select(["id", "username", "email", "password"])
-      .from ("users")
+      .select(["id", "email", "password"])
+      .from("users")
       .where({ email });
-    
-      if (!userArray.length) {
-        return res.status(404).send("Email does not exist");
-      }
 
-      const isValidPassword = await bcrypt.compare(password, userArray[0].password);
+    if (!userArray.length) {
+      return res.status(404).send("Email does not exist");
+    }
 
-      if(!isValidPassword) return res.status(401).send("Incorrect password")
+    const isValidPassword = await bcrypt.compare(
+      password,
+      userArray[0].password
+    );
 
-      const token = auth.createToken(userArray[0].id);
-     
-      const loginObject = {
-        token,
-        username: userArray[0].username,
-        email: userArray[0].email,
-      }
+    if (!isValidPassword) return res.status(401).send("Incorrect password");
 
-      return res.status(200).json(loginObject);
+    const token = auth.createToken(userArray[0].id);
+
+    const loginObject = {
+      token,
+      email: userArray[0].email,
+    };
+
+    return res.status(200).json(loginObject);
   } catch (error) {
     return error;
   }
@@ -79,5 +67,5 @@ const login = async(req, res) => {
 
 module.exports = {
   signup,
-  login
+  login,
 };
